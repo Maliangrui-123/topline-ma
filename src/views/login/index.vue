1ab5b19a956418de2333d02f06a786a310d6a8dd
Maliang <965932942@qq.com>
2020-01-11 19:24:17 +0800

    实现登录功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,8 +5,10 @@
     <!-- 导航栏结束 -->
     <!-- 登录表单 -->
     <van-cell-group>
-      <van-field v-model="user.mobile" required clearable label="手机号" placeholder="请输入手机号" />
-      <van-field v-model="user.code" type="password" label="验证码" placeholder="请输入验证码" />
+      <van-field v-model="user.mobile" left-icon="contact" clearable placeholder="请输入手机号" />
+      <van-field v-model="user.code" placeholder="请输入验证码" required>
+        <van-button round slot="button" size="small" type="primary">发送验证码</van-button>
+      </van-field>
     </van-cell-group>
     <!-- 登录表单结束 -->
     <!-- 登录按钮 -->
@@ -18,6 +20,8 @@
 </template>
 
 <script>
+import request from '@/utils/request'
+
 export default {
   name: 'LoginPage',
   components: {},
@@ -25,15 +29,31 @@ export default {
   data () {
     return {
       user: {
-        mobile: '',
-        code: ''
+        mobile: '', // 手机号
+        code: '' // 验证码
       }
     }
   },
   computed: {},
   watch: {},
   created () {},
-  methods: {}
+  methods: {
+    async onLogin () {
+      // 1.获取表单数据
+      const user = this.user
+
+      // 2.表单验证
+
+      // 3.请求登录
+      const res = await request({
+        method: 'POST',
+        url: '/app/v1_0/authorizations',
+        data: user // 请求体参数
+      })
+      console.log(res)
+      // 4.根据后端返回结果执行处理
+    }
+  }
 }
 </script>
 
